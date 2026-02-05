0655d665c1c2e14e796b8151aecf4997084ab134
Daniel Weck <daniel.weck@gmail.com>
2026-02-05 11:53:08 +0000

    mods

--- etc/install-libsk-libfido2-v1.1.5.zsh
+++ etc/install-libsk-libfido2-v1.1.5.zsh
@@ -21,27 +21,23 @@ fi
 
 cp $src_lib_path $dest_lib_path
 
-cat <<EOF | tee /Library/LaunchAgents/com.mroosz.ssh_env_vars.plist
+cat <<EOF | tee /Library/LaunchAgents/com.danielweck.ssh_env_vars.plist
 <?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
 <plist version="1.0">
 <dict>
 	<key>EnvironmentVariables</key>
 	<dict>
-		<key>SSH_ASKPASS</key>
-		<string>${homebrew_prefix}/bin/ssh-askpass</string>
-		<key>SSH_ASKPASS_REQUIRE</key>
-		<string>force</string>
 		<key>SSH_SK_PROVIDER</key>
 		<string>/usr/local/lib/libsk-libfido2.dylib</string>
 	</dict>
 	<key>Label</key>
-	<string>com.mroosz.ssh_env_vars</string>
+	<string>com.danielweck.ssh_env_vars</string>
 	<key>ProgramArguments</key>
 	<array>
 		<string>/bin/zsh</string>
 		<string>-c</string>
-		<string>/bin/launchctl setenv SSH_ASKPASS ${homebrew_prefix}/bin/ssh-askpass; /bin/launchctl setenv SSH_SK_PROVIDER /usr/local/lib/libsk-libfido2.dylib; killall ssh-agent; /bin/ln -sf \$SSH_AUTH_SOCK_LOCAL \$SSH_AUTH_SOCK; SSH_AUTH_SOCK=\$SSH_AUTH_SOCK_LOCAL /usr/bin/ssh-agent -l</string>
+		<string>/bin/launchctl setenv SSH_SK_PROVIDER /usr/local/lib/libsk-libfido2.dylib; killall ssh-agent; /bin/ln -sf \$SSH_AUTH_SOCK_LOCAL \$SSH_AUTH_SOCK; SSH_AUTH_SOCK=\$SSH_AUTH_SOCK_LOCAL /usr/bin/ssh-agent -l</string>
 	</array>
 	<key>RunAtLoad</key>
 	<true/>
