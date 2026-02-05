cf67596453830234f9014816a1ce4d29109600c6
Daniel Weck <daniel.weck@gmail.com>
2026-02-05 11:44:59 +0000

    mods

--- Formula/sshpass.rb
+++ Formula/sshpass.rb
@@ -1,19 +1,1 @@
-class Sshpass < Formula
-  desc "Non-interactive ssh password auth Files"
-  homepage "https://sourceforge.net/projects/sshpass/"
-  url "https://github.com/MichaelRoosz/sshpass/releases/download/v.1.10-mr/sshpass-1.10-mr.tar.gz"
-  sha256 "4ce41b3a953af86fad11977ec4656ca4f98b8d1c8d9cd399c7aeb3d1a7c8957d"
-  license "GPL-2.0-or-later"
 
-  def install
-    system "./configure", *std_configure_args, "--disable-silent-rules"
-    system "make", "install"
-  end
-
-  test do
-    assert_match "ssh: Could not resolve hostname host: nodename nor servname provided, or not known",
-      shell_output("#{bin}/sshpass -p mypassword ssh username@host touch foo 2>&1", 255)
-
-    assert_match "sshpass #{version}", shell_output("#{bin}/sshpass -V")
-  end
-end
